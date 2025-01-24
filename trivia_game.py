984464c13fa95891eff9127c9487036695383be2
StavrosV28 <svarvatsoulis@gmail.com>
2025-01-23 19:06:30 -0500

    Added user input to display, questions one by one. Added checking user input for the correct answer. Lastly, correction if user enters a number out of range.

--- trivia_game.py
+++ trivia_game.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 
 # Just a message to start the game up and introduce the rules etc.
@@ -15,20 +16,49 @@ def load_questions(file_path):
         questions = json.load(file)
     return questions['results']
 
+
 def display_questions(questions):
+    # Display the questions on the command line
     for idx, question_data  in enumerate(questions, start=1):
         print(f'Question {idx}: {question_data['question']}')
-
-
-
-
+        answers = question_data['incorrect_answers'] + [question_data['correct_answer']]
+        # Display answer choices to user on command line
+        for i, option in enumerate(answers, start=1):
+            print(f'{i}: {option}')
+        # Checking if the user enters a number out of range of the answer choices
+        while True:
+            try:
+                player_answer = int(input(f'Give an answer choice of 1 through {len(answers)}: '))
+                if 1 <= player_answer <= len(answers):
+                    break
+                else:
+                    print(f'Please enter a number between 1 through {len(answers)}')
+            except ValueError:
+                print('Invalid input. Please enter a valid answer choice.')
+        
+        # Check if answer is correct that the user entered.
+        if answers[player_answer - 1] == question_data['correct_answer']:
+            print('Correct Answer...On to the next!')
+        else: 
+            print('Incorrect! Next question please.')
+    
+        
+    
+        
+            
+        
+    
+    
+    
 
 
 # For testing
 def main():
     file_path = 'questionsdb.json'
     questions = load_questions(file_path)
-    display_questions(questions)
+    welcome = welcome_UI()
+    welcome # Displays Welcome UI
+    display_questions(questions) # Shows the questions and their order
     
     
 if __name__ == '__main__':
