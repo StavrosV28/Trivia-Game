272c97edc90263a46a4b64f2d35a537607091f64
StavrosV28 <svarvatsoulis@gmail.com>
2025-01-28 17:42:16 -0500

    Cleaned up code by adding every function in a class

--- trivia_game.py
+++ trivia_game.py
@@ -1,80 +1,81 @@
 import json
 import random
 
-
-# Just a message to start the game up and introduce the rules etc.
-def welcome_UI():
-    print('Welcome to the best trivia game on earth!')
-    print()
-    print('These questions will be based on video games.')
-    print()
-    print('There are 3 types of question difficulties. Easy, Medium, and Hard.')
-    print()
-    print('Easy = 5 pts, Medium = 10 pts, and Hard = 15 pts')
-    print()
-    
-
-
-# Load the questions from the JSON file
-def load_questions(file_path):
-    with open(file_path, 'r')as file:
-        questions = json.load(file)
-    return questions['results']
-
-# Displays questions along with their answer choices
-def display_questions(questions):
-    # Display the questions on the command line
-    for idx, question_data  in enumerate(questions, start=1):
-        print(f'Question {idx}: {question_data['question']}')
-        answers = question_data['incorrect_answers'] + [question_data['correct_answer']]
+class TriviaGame:
+    # Just a message to start the game up and introduce the rules etc.
+    def welcome_UI():
+        print('Welcome to the best trivia game on earth!')
+        print()
+        print('These questions will be based on video games.')
+        print()
+        print('There are 3 types of question difficulties. Easy, Medium, and Hard.')
+        print()
+        print('Easy = 5 pts, Medium = 10 pts, and Hard = 15 pts')
+        print()
         
-        # Display answer choices to user on command line
-        for i, option in enumerate(answers, start=1):
-            print(f'{i}: {option}')
+
+
+    # Load the questions from the JSON file
+    def load_questions(file_path):
+        with open(file_path, 'r')as file:
+            questions = json.load(file)
+        return questions['results']
+        
+        
+    # Displays questions along with their answer choices
+    def display_questions(questions):
+        # Display the questions on the command line
+        for idx, question_data  in enumerate(questions, start=1):
+            print(f'Question {idx}: {question_data['question']}')
+            answers = question_data['incorrect_answers'] + [question_data['correct_answer']]
             
-        # Checking if the user enters a number out of range of the answer choices
+            # Display answer choices to user on command line
+            for i, option in enumerate(answers, start=1):
+                print(f'{i}: {option}')
+                
+            # Checking if the user enters a number out of range of the answer choices
+            while True:
+                try:
+                    player_answer = int(input(f'Give an answer choice of 1 through {len(answers)}: '))
+                    if 1 <= player_answer <= len(answers):
+                        break
+                    else:
+                        print(f'Please enter a number between 1 through {len(answers)}')
+                except ValueError:
+                    print('Invalid input. Please enter a valid answer choice.')
+            
+            # Check if answer is correct that the user entered.
+            if answers[player_answer - 1] == question_data['correct_answer']:
+                print('Correct Answer...On to the next!')
+            else: 
+                print(f'Incorrect! The correct answer was {question_data['correct_answer']}.')
+
+            
+        
+    # For testing
+    def main():
+        file_path = 'questionsdb.json'
+        questions = load_questions(file_path)
+        random.shuffle(questions)
+        welcome = welcome_UI()
+        
+        # Asking user to see how many questions they would like to answer from the database of questions.
         while True:
             try:
-                player_answer = int(input(f'Give an answer choice of 1 through {len(answers)}: '))
-                if 1 <= player_answer <= len(answers):
+                questions_num = int(input(f'How many questions would you like to answer? (1-{len(questions)}): '))
+                if 1 <= questions_num <= len(questions):
                     break
                 else:
-                    print(f'Please enter a number between 1 through {len(answers)}')
+                    print(f'Please enter a number between 1 through {len(questions)}')
             except ValueError:
-                print('Invalid input. Please enter a valid answer choice.')
+                print('Out of range! Please enter a valid range.')
         
-        # Check if answer is correct that the user entered.
-        if answers[player_answer - 1] == question_data['correct_answer']:
-            print('Correct Answer...On to the next!')
-        else: 
-            print(f'Incorrect! The correct answer was {question_data['correct_answer']}.')
-    
-    
-# For testing
-def main():
-    file_path = 'questionsdb.json'
-    questions = load_questions(file_path)
-    random.shuffle(questions)
-    welcome = welcome_UI()
-    
-    # Asking user to see how many questions they would like to answer from the database of questions.
-    while True:
-        try:
-            questions_num = int(input(f'How many questions would you like to answer? (1-{len(questions)}): '))
-            if 1 <= questions_num <= len(questions):
-                break
-            else:
-                print(f'Please enter a number between 1 through {len(questions)}')
-        except ValueError:
-            print('Out of range! Please enter a valid range.')
-    
-    selected_questions = questions[:questions_num]
-    
-    welcome # Displays Welcome UI
-    display_questions(selected_questions) # Shows the questions and their order
-    print('Thanks for playing!')
-    
-    
-if __name__ == '__main__':
-    main()
+        selected_questions = questions[:questions_num]
+        
+        welcome # Displays Welcome UI
+        display_questions(selected_questions) # Shows the questions and their order
+        print('Thanks for playing!')
+        
+    if __name__ == '__main__':
+        main()
 
