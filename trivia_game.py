4455c8b8c79bbe738dd05c03c138e8dfa89d8c2d
StavrosV28 <svarvatsoulis@gmail.com>
2025-02-07 17:46:40 -0500

    Fixed some logic for adding points to the user

--- trivia_game.py
+++ trivia_game.py
@@ -25,19 +25,22 @@ def load_questions(file_path):
     
 # Displays questions along with their answer choices
 def display_questions(questions):
+    score_map = {'easy': 3, 'medium': 5, 'hard': 10}
     player_score = 0
     
     # Display the questions on the command line
     for index, question_data  in enumerate(questions, start=1):
+        print('-' * 50)
         # question_text will decode the HTML entities from the JSON file
         question_text = html.unescape(question_data['question'])
-        
         print(f'Question {index}: {question_text}')
         
         answers = question_data['incorrect_answers'] + [question_data['correct_answer']]
-        answers_text = html.unescape(answers)
+        answers = [html.unescape(ans) for ans in answers]
+        random.shuffle(answers)
         # Display answer choices to user on command line
-        for i, option in enumerate(answers_text, start=1):
+        
+        for i, option in enumerate(answers, start=1):
             print(f'{i}: {option}')
         
         question_difficulty = question_data['difficulty']
@@ -56,21 +59,11 @@ def display_questions(questions):
         # Check if answer is correct that the user entered.
         if answers[player_answer - 1] == question_data['correct_answer']:
             print('Correct Answer...On to the next!')
-            # Update players score as they progress through
-            if question_difficulty == 'easy':
-                player_score += 3
-                print(f'You gained 3 points. Your current score is: {player_score}')
-                print()
-            elif question_difficulty == 'medium':
-                player_score += 5
-                print(f'You gained 5 points. Your current score is: {player_score}')
-                print()
-            elif question_difficulty == 'hard':
-                player_score += 10
-                print(f'You gained 10 points. Your current score is: {player_score}')
-                print()
-        else: 
-            print(f'Incorrect! The correct answer was {question_data['correct_answer']}.')
+            points = score_map.get(question_data['difficulty'], 0)
+            player_score += points
+            print(f'You gained {points} points. Total score: {player_score}\n')
+        else:
+            print(f'Incorrect. The correct answer was: {question_data["correct_answer"]}\n')
             
     print(f'Your total score was: {player_score}')
         
@@ -96,7 +89,9 @@ def main():
     # Slice the questions up to the desired amount of questions the player wants
     selected_questions = questions[:questions_num]
     
-    welcome # Displays Welcome UI
+    welcome = welcome_UI # Displays Welcome UI
+    
+    welcome
     display_questions(selected_questions) # Shows the questions and their order
     print('Thanks for playing!')
     
